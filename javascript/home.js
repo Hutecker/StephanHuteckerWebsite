e6fce84dd6db7f5bc078bff454b3e0f5186b0fff
Hutecker <hutecker@msu.edu>
2015-06-25 22:59:19 -0500

    Pages are now selected with angular, contact form is working with live preview, code for sending emails via php is there but not tested

--- javascript/home.js
+++ javascript/home.js
@@ -1,6 +1,6 @@
 var app = angular.module('HomePage', ['ui.bootstrap']);
 
-app.controller('NavBarController', function ($scope, $location) {
+app.controller('PageContentController', function ($scope, $location) {
 
     //Used to redirect urls
     $scope.isActive = function (viewLocation) {
@@ -9,5 +9,54 @@ app.controller('NavBarController', function ($scope, $location) {
 
     //Used to collapse nav bar button
     $scope.isCollapsed = true;
+
+    //This string determines what page is loaded
+    $scope.currentPage = "Home";
+});
+
+app.controller('HomePageController', function ($scope, $location) {
+
+
+});
+
+app.controller('AboutPageController', function ($scope, $location) {
+
+});
+
+app.controller('ContactPageController', function ($scope, $location) {
+
+    //Basic information about the user
+    $scope.email = "";
+    $scope.firstName = "";
+    $scope.lastName = "";
+    $scope.message = "";
+
+    //Creates footer
+    $scope.getFooter = function () {
+        return "Best,\n" + $scope.firstName + " " + $scope.lastName;
+    }
+
+    //Creates entire message
+    $scope.createMessage = function () {
+        if ($scope.message == "")
+            return "";
+     
+        return $scope.message + "\n\n" + $scope.getFooter();
+    }
+
+    //Sends out the email
+    $scope.submit = function () {
+        $.ajax({
+            url: 'PHP/MailHandler.php',
+            data: {to: $scope.email, message: $scope.createMessage()},
+            success: function (response) {
+                //display success alert
+            },
+            error: function () {
+                //display error
+            }
+        });
+    }
+
 });
 
